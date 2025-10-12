d0a9a5967734fb8495e6aff7942630aca52eb8cc
Syd <sydney@LAPTOP-38R957I6.localdomain>
2025-10-11 22:16:46 -0500

    Hopefully Finalized Row 3

--- OrderedRamsey-Table-7/Upper/Table7Row3/round.sage
+++ OrderedRamsey-Table-7/Upper/Table7Row3/round.sage
@@ -1,8 +1,6 @@
 import sys
 load("../rounding_Integer.sage")
-round_program("SDP_n"+sys.argv+"_LB_F_ordered_edges3__objective.txt.dat-s",scale=10)
-'''
-Result:
-Minimum (after unscaling) found at 4160 / 6001 Value is 0.031721852257217624
-317218522572176250076683392285154591886800 / 10000000000000000000000000000000000000000000  # =  0.031721852257217624
-'''
+S = 10
+if int(sys.argv[1]) == 6:
+	S = 30
+round_program("SDP_n" + str(sys.argv[1]) + "_LB_F_ordered_edges3__objective.txt.dat-s",scale=S)
--- OrderedRamsey-Table-7/Upper/Table7Row3/run.sh
+++ OrderedRamsey-Table-7/Upper/Table7Row3/run.sh
@@ -1,5 +1,23 @@
+#!/bin/bash
+
+echo "begin"
 python3 ../general_generate.py  '1 1 0 0  0 0 1  1 0  0' > F_ordered_edges3__forbidden.txt
 printf "3 0 1 2 3\n3 0 1 3 2\n3 0 2 1 3\n3 0 2 3 1\n3 0 3 1 2\n3 0 3 2 1\n" >> F_ordered_edges3__forbidden.txt
-printf "3 0 1 1 2\n3 0 1 2 1\n3 0 2 1 1\n 3 0 1 1 3\n3 0 1 3 1\n3 0 3 1 1" >> F_ordered_edges3__forbidden.txt
+printf "3 0 1 1 2\n3 0 1 2 1\n3 0 2 1 1\n3 0 1 1 3\n3 0 1 3 1\n3 0 3 1 1" >> F_ordered_edges3__forbidden.txt
+if [ $1 -eq 5 ]
+then
 printf "10 2 0 1\n" > F_ordered_edges3__objective.txt
-../a.out -n $1 -lb &> log$1.txt
+else
+printf "30 2 0 1\n" > F_ordered_edges3__objective.txt
+fi
+echo "constraints set"
+rm  n$1/*
+echo "Start solving"
+../a.out -n $1 -lb -fgf &> n$1/solver_log_$1.txt
+echo "Solving finished"
+echo "Start Rounding"
+sage round.sage $1 $> n$1/rounding_log_$1.txt
+echo "Rounding finished"
+mv SDP_n$1* n$1
+zip -r n$1.zip n$1
+echo "Done"
